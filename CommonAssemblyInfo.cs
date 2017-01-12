bc6948f414799d4b06a5c42fe59c8e01a5b8a504
Tatarincev <et@virtoway.com>
2017-01-12 13:07:10 +0200

    Add CommonAssemblyInfo

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("VirtoCommerce")]
+[assembly: AssemblyProduct("VirtoCommerce Subscription Module")]
+[assembly: AssemblyCopyright("Copyright © VirtoCommerce 2011-2017")]
+
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+
+#if DEBUG
+
+[assembly: AssemblyConfiguration("Debug")]
+
+#else
+[assembly: AssemblyConfiguration("Release")]
+
+#endif
+
+
+[assembly: ComVisible(false)]
+
+[assembly: CLSCompliant(true)]
