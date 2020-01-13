79038ed73f041286cf87907a5cb19a6ab94bbc18
Andrey Kostyrin <kostyrin2@gmail.com>
2020-01-13 13:04:51 +0200

    #vp-864 migrating to core3 (#25)
    Adapted to ASP.NET CORE3

--- src/VirtoCommerce.SubscriptionModule.Data/Repositories/SubscriptionDbContext.cs
+++ src/VirtoCommerce.SubscriptionModule.Data/Repositories/SubscriptionDbContext.cs
@@ -20,12 +20,12 @@ namespace VirtoCommerce.SubscriptionModule.Data.Repositories
         {
             #region Subscription        
             modelBuilder.Entity<SubscriptionEntity>().ToTable("Subscription").HasKey(x => x.Id);
-            modelBuilder.Entity<SubscriptionEntity>().Property(x => x.Id).HasMaxLength(128);
+            modelBuilder.Entity<SubscriptionEntity>().Property(x => x.Id).HasMaxLength(128).ValueGeneratedOnAdd();
             #endregion
 
             #region PaymentPlan
             modelBuilder.Entity<PaymentPlanEntity>().ToTable("PaymentPlan").HasKey(x => x.Id);
-            modelBuilder.Entity<PaymentPlanEntity>().Property(x => x.Id).HasMaxLength(128);
+            modelBuilder.Entity<PaymentPlanEntity>().Property(x => x.Id).HasMaxLength(128).ValueGeneratedOnAdd();
             #endregion
 
             base.OnModelCreating(modelBuilder);
