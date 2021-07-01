9960e47a632c6e608bdfc896d92fa7e68076c3cc
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2021-07-01 09:57:04 +0200

    PT-2765 fix smell

--- src/VirtoCommerce.SubscriptionModule.Web/Scripts/widgets/subscription-orders-widget.js
+++ src/VirtoCommerce.SubscriptionModule.Web/Scripts/widgets/subscription-orders-widget.js
@@ -10,15 +10,19 @@ angular.module('virtoCommerce.subscriptionModule')
                         title: 'subscription.blades.subscriptionOrder-list.title',
                         controller: 'virtoCommerce.orderModule.customerOrderListController',
                         template: 'Modules/$(VirtoCommerce.Orders)/Scripts/blades/customerOrder-list.tpl.html',
-                        hideDelete: true,
-                        refreshCallback: function (blade) {
+                        hideDelete: true
+                    };
+
+                    if (blade.currentEntity.id) {
+                        newBlade.refreshCallback = function () {
                             var criteria = {
-                                subscriptionIds: [blade.parentBlade.currentEntity.id]
+                                subscriptionIds: [blade.currentEntity.id]
                             };
-                            
+
                             return orders.search(criteria);
                         }
-                    };
+                    }
+
                     bladeNavigationService.showBlade(newBlade, blade);
                 }
             };
