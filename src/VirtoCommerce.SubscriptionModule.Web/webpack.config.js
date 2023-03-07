ae5403edfc0f83fc60f793e8bdb63718fe4674a8
Oleg Zhuk <zhukoo@gmail.com>
2023-03-07 19:23:47 +0200

    feat: PT-9395 add nodejs-lts support

--- src/VirtoCommerce.SubscriptionModule.Web/webpack.config.js
+++ src/VirtoCommerce.SubscriptionModule.Web/webpack.config.js
@@ -1,19 +1,54 @@
+const moduleId = "VirtoCommerce.Subscription"
+
 const glob = require("glob");
 const path = require("path");
 const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-module.exports = [
-    {
-        entry: glob.sync('./Scripts/**/*.js', { nosort: true }),
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: 'app.js'
-        },
+const rootPath = path.resolve(__dirname, "dist");
+
+function getEntrypoints() {
+    const result = [
+        ...glob.sync("./Scripts/**/*.js", { nosort: true }),
+        ...glob.sync("./Content/**/*.css", { nosort: true })
+    ];
+
+    return result;
+}
+
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === "production";
+
+    return {
+        entry: getEntrypoints(),
         devtool: false,
+        output: {
+            path: rootPath,
+            filename: "app.js"
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: [MiniCssExtractPlugin.loader, "css-loader"]
+                }
+            ]
+        },
         plugins: [
-            new webpack.SourceMapDevToolPlugin({
-                namespace: 'VirtoCommerce.Subscription'
+            new CleanWebpackPlugin(),
+            isProduction ?
+                new webpack.SourceMapDevToolPlugin({
+                    namespace: moduleId,
+                    filename: "[file].map[query]"
+                }) :
+                new webpack.SourceMapDevToolPlugin({
+                    namespace: moduleId
+                }),
+            new MiniCssExtractPlugin({
+                filename: "style.css"
             })
         ]
-    }
-];
+    };
+};
+
